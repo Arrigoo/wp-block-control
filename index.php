e4be11c023ff8c1f1aa82a9de82a4c91b77d5317
Andreas B. Vestergaard <51328177+Abvpeytz@users.noreply.github.com>
2025-10-29 10:04:20 +0100

    Add custom block support (#3)
    * Stop tracking node_modules and package-lock.json
    
    * fix(plugin): ignored relevant files

--- index.php
+++ index.php
@@ -8,13 +8,14 @@
  */
 
 // In case we are in a non-composer environment.
-if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    require_once __DIR__ . '/vendor/autoload.php';
+if ( file_exists(__DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
 }
 
 require_once __DIR__ . '/src_php/BlockControl.php';
 require_once __DIR__ . '/src_php/EndUser.php';
 
-add_action('wp_head', '\Arrigoo\WpCdpBlockControl\EndUser::arrigoo_cdp_custom_javascript');
+add_action('wp_head', '\Arrigoo\WpCdpBlockControl\EndUser::arrigoo_cdp_custom_javascript' );
 add_filter( 'register_block_type_args', '\Arrigoo\WpCdpBlockControl\BlockControl::arrigoo_cdp_add_segments_attribute_to_blocks', 10, 2 );
-add_action('enqueue_block_editor_assets', 'Arrigoo\WpCdpBlockControl\BlockControl::arrigoo_segment_block_control_enqueue_assets');
+add_action('enqueue_block_editor_assets', 'Arrigoo\WpCdpBlockControl\BlockControl::arrigoo_segment_block_control_enqueue_assets' );
+add_filter( 'render_block', '\Arrigoo\WpCdpBlockControl\BlockControl::arrigoo_add_segment_attributes_to_dynamic_blocks', 10, 2 );
