bb7435728609f175c5c7c2ccb22442aaea1eed51
Andreas B. Vestergaard <abv@peytz.dk>
2025-10-29 09:08:20 +0100

    Stop tracking node_modules and package-lock.json

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks', 'wp-i18n'), 'version' => '736d379232a9b1014cb3');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-hooks', 'wp-i18n'), 'version' => 'cc73e2a3b13317e69bfe');
