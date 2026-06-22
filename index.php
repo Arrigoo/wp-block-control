c138db98230364920873155fb3c99a37d4d1328b
Kristougher <kristoffer@arrigoo.io>
2026-06-22 12:51:46 +0200

    Updated version number.

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /*
  * Plugin Name: Arrigoo CDP Block control
  * Description: Adds a custom control to all Gutenberg blocks to hide/show the block from different segments.
- * Version: 1.2.7
+ * Version: 1.2.9
  * Author: Arrigoo.io
  */
 
